716717ed9eec3f878a52f1385a8489c4795ab0a5
Mukeshjha211 <114067075+Mukeshjha211@users.noreply.github.com>
2023-03-30 15:28:24 +0530

    Add files via upload
    Making the Data visible on the screen.

--- server.js
+++ server.js
@@ -1,16 +1,41 @@
 const http = require('http');
+const fs = require('fs');
+const { buffer } = require('stream/consumers');
 
 const server = http.createServer((req, res)=>{
-    console.log(req.url, req.method, req.headers
-        );
-        // process.exit();
-
-    res.setHeader('Content-Type', 'text/html');
+   const url = req.url;
+   const method = req.method;
+   if(url === '/'){
+      fs.readFile('message.txt', {encoding: 'utf-8'}, (err, data)=>{
+         if(err){
+            console.log(err);
+         }
     res.write('<html>');
-    res.write('<head><title>My First Page</title><head>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1><body>');
+    res.write('<head><title>Enter Message</title><head>');
+    res.write(`<body>${data}</body>`)
+    res.write('<body><form action="/message" method = "POST"><input type= "text" name="message"><button type="submit">Send</button></form></body>');
     res.write('</html>');
-    res.end();
+    return res.end();
+ 
+   });
+}
+
+   else if(url === '/message' && method === 'POST'){
+    const body = [];
+    req.on('data', (chunk) =>{
+      
+      body.push(chunk);
+    } );
+    req.on('end', ()=>{
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+      
+    })
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+   }
    })
 
-server.listen(3001);
+server.listen(3005);
